a07cb591ec0363f003a0fda84bd8f3022ed16dfb
wasim2263 <abdullahwasim42@gmail.com>
2023-02-06 14:41:10 +0600

    country list added and fixed all columns

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -1,23 +1,91 @@
-import {useEffect, useState} from "react";
+import {ReactNode, useEffect, useState} from "react";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import axios from "axios";
-import {Table, TableBody, TableCell, TableContainer, TablePagination, TableRow} from "@mui/material";
+import {Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow} from "@mui/material";
+interface Column {
+    id: 'flag' | 'name' | 'region' | 'population' | 'languages' | "details";
+    label: string;
+    minWidth?: number;
+    align?: 'right'|'left';
+    format?: (value: string | number) => string;
+}
+
+const columns: readonly Column[] = [
+    {id: 'flag', label: 'Flag', minWidth: 100},
+    {id: 'name', label: 'Name', minWidth: 100},
+    {
+        id: 'region',
+        label: 'Region',
+        minWidth: 100,
+    },
+    {
+        id: 'population',
+        label: 'Population',
+        minWidth: 100,
+        align: 'left',
+        format: (value: string | number) => value.toLocaleString('en-US'),
+    },
+    {
+        id: 'languages',
+        label: 'Languages',
+        minWidth: 100,
+        align: 'left',
+    },
+    {id: 'details', label: '', minWidth: 100, align: 'right'},
+
+];
+
+interface Data {
+    flag: string;
+    name: string;
+    region: string;
+    population: number;
+    languages: ReactNode;
+    details: ReactNode;
+}
+
+function createData(
+    flag: string,
+    name: string,
+    region: string,
+    population: number,
+    languagesObject: Record<string, string>,
+): Data {
+    let languages = <LanguageList languages={languagesObject}/>;
+    const details = <a href={name}> <ChevronRightIcon sx={{mr: 2}}/> </a>;
+    return {flag, name, region, population, languages, details};
+}
 
 type country = {
-    name: {
-        common: string;
-    };
     coatOfArms: {
         svg: string;
     };
+    name: {
+        common: string;
+    };
+
+    region: string;
+    population: number;
+    languages: Record<string, string>;
+
 };
+type LanguagesProps = { languages: Record<string, string> };
+const LanguageList = ({languages}:LanguagesProps) => {
+    return (
+        <ul>
+            {Object.values(languages).map((language,index)=><li key={index}>{language}</li>)}
+        </ul>
+    )
+}
 export const Country = () => {
-    const [countries, setCountries] = useState<country[]>([])
+    const [countries, setCountries] = useState<Data[]>([])
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(5);
     const hook = () => {
         const eventHandler = (response: any) => {
             console.log('promise fulfilled')
-            setCountries(response.data)
+            const countries = response.data.map((country: country) => createData(country.coatOfArms.svg, country.name.common, country.region, country.population, country.languages))
+            setCountries(countries)
         }
         const promise = axios.get('https://restcountries.com/v3.1/all')
         promise.then(eventHandler)
@@ -33,28 +101,42 @@ export const Country = () => {
     };
     return (
         <>
-            {console.log('render', countries.length, 'notes')}
-
             <TableContainer sx={{maxHeight: 440}}>
                 <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column) => (
+                                <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    style={{minWidth: column.minWidth}}
+                                >
+                                    {column.label}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
                     <TableBody>
                         {countries
                             .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((row) => {
+                            .map((country) => {
                                 return (
-                                    <TableRow hover role="checkbox" tabIndex={-1} key={row?.name?.common}>
-                                        <TableCell>
-                                            <img
-                                                src={row?.coatOfArms?.svg}
-                                                height={"50px"}
-                                                width={"auto"}
-                                            />
+                                    <TableRow hover role="checkbox" tabIndex={-1} key={country.name}>
+                                        {columns.map((column) => {
+                                            const value = country[column.id];
+                                            const type = typeof value;
+                                            console.log(type)
+                                            return (
+                                                <TableCell key={column.id} align={column.align}>
+                                                    {column.format && type === 'number'
+                                                        ? column.format(value+"")
+                                                        : column.id !== 'flag'
+                                                            ? value
+                                                            : <img src={value+""} height={"50px"} width={"auto"}/>}
+                                                </TableCell>
 
-                                        </TableCell>
-                                        <TableCell>
-                                            {row?.name?.common}
-                                        </TableCell>
-
+                                            );
+                                        })}
                                     </TableRow>
                                 );
                             })}
